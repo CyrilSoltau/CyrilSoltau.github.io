f3fd78cfdac421aaf73b17d96f5fb5ba224c5916
CyrilSoltau <113400067+CyrilSoltau@users.noreply.github.com>
2022-12-12 13:44:20 -0700

    Adding updated simply lawns files

---
+++ Simply_Lawns/javascript/landingscript.js
@@ -0,0 +1,3 @@
+"use strict";
+//Change how the message is displayed.
+<!--alert("Thank you for visiting. More features and pages will be coming over the following weeks. \n  \u2665 Simply Lawns"); //>
