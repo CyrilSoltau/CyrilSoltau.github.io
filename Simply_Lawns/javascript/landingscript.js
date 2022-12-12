50ba897f392799775881dc70fb8f3f9f2e828b45
CyrilSoltau <113400067+CyrilSoltau@users.noreply.github.com>
2022-12-12 13:42:45 -0700

    Delete Simply_Lawns directory

--- Simply_Lawns/javascript/landingscript.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-//Change how the message is displayed.
-<!--alert("Thank you for visiting. More features and pages will be coming over the following weeks. \n  \u2665 Simply Lawns"); //>
