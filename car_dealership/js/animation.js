0c615993455d8e6bd84b928655a7464710b53528
CyrilSoltau <113400067+CyrilSoltau@users.noreply.github.com>
2022-12-15 08:32:40 -0700

    Add files via upload

---
+++ car_dealership/js/animation.js
@@ -0,0 +1,13 @@
+"use strict";
+$(document).ready( () => {
+	
+	
+    
+    // runs when the page is ready
+    $(" h1").animate( { fontSize: "650%", opacity: 1, left: "+=375" }, 1000 )  
+		         .animate( { fontSize: "650%", left: 0 }, 1000 );
+		    
+	
+	
+    
+}); // end ready
